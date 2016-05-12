c8fc80e95be5960dc6bb31dac74d2b0f69f0751c
JuanDiegoArango <arangojuan1993@gmail.com>
2016-05-12 12:17:58 -0500

    unos arreglos
    final

--- MCMC.py
+++ MCMC.py
@@ -1,26 +1,18 @@
-
-# coding: utf-8
-
-# In[149]:
-
 import numpy as np
-import matplotlib as plt
+import matplotlib.pyplot as plt
 
 
-# In[150]:
 
 coordenadas=np.loadtxt("coordenadas.dat", delimiter=',')
 numero_de_particulas=len((coordenadas))
 
 
-# In[ ]:
 
 #costantes
 G=4.492e-3
-m=
+m=1
 
 
-# In[2]:
 
 def potencial_gravitacional(r):
     return -G*m*m/r
@@ -35,7 +27,6 @@ def nuevo_valor(x,sigma):
     return np.random.normal(x, sigma)
 
 
-# In[ ]:
 
 n=1000 #iteraciones
 indices=range(0,numero_de_particulas)
@@ -43,7 +34,6 @@ potenciales=[]
 loglike=[]
 
 
-# In[ ]:
 
 for i in indices:
     suma=0
@@ -58,7 +48,6 @@ print centro
 print "tiene el mayor potencial entonces es la que se encuentra mas central a todas las demas particulas"     
 
 
-# In[ ]:
 
 coordenadas=coordenadas-coordenadas[centro]
 #particula mas lejana
@@ -77,17 +66,6 @@ distancias=array(distancias)
     
 
 
-# In[3]:
-
-a=0.001 #exponente alfa
-b=15.0 #exponente beta
-logr0=10.0
-logrho=50.0
-distancias=np.exp(np.linspace(-20.0,60,20000))
-y_obs=densidad(distancias, a, b,logr0,logrho)
-
-
-# In[9]:
 
 parametro1=[]#exponente alfa
 parametro2=[]#exponente beta
@@ -147,9 +125,7 @@ while(i<n):
         parametro3.append(parametro_prueba[2])
         parametro4.append(parametro_prueba[3])
         parametro=[parametro_prueba[0],parametro_prueba[1],parametro_prueba[2],parametro_prueba[3]]
-        likehood.append(likehood_nuevo)
-        print i
-        
+        likehood.append(likehood_nuevo)        
         
     else:
         parametro1.append(parametro1[i])
@@ -163,88 +139,17 @@ while(i<n):
 
     i=i+1
 
-
-# In[ ]:
-
-#se ha visto que hay problemas en hacer un ajuste para el parametro beta que es el responsable de como de cae la densidad
-#entonces se hace otra cadena de marvok con solo beta para ajustar mejor el parametro
-#tomando los otros parametros como fijos
-parametro_beta=[]#exponente beta
-likehood=[]
-parametro_beta.append(parametro[1])
-parametro_prueba=0
-nuevo_b=0
-
-i=0
-n=10000
-
-while(i<n):      
-    beta=(nuevo_valor(parametro_beta[i],10.0))
-    if(beta>0):
-            parametro_prueba=(beta)
-    else:        
-        while(beta<0):
-            beta=(nuevo_valor(parametro_beta[i],10.0))
-            if(beta>0):
-                parametro_prueba=(beta)
-                           
-    y_modelo_nuevo = densidad(distancias,parametro[0],parametro_prueba,parametro[2],parametro[3])
-    y_modelo_viejo = densidad(distancias,parametro[0],parametro_beta[i],parametro[2],parametro[3])
-
-    likehood_nuevo=loglikelihood(y_obs,y_modelo_nuevo)
-    likehood_viejo=loglikelihood(y_obs,y_modelo_viejo)    
-    delta=min(1,np.exp(-likehood_viejo+likehood_nuevo)) 
-    
-    if(np.random.uniform()<=delta):
-        parametro_beta.append(parametro_prueba)
-        likehood.append(likehood_nuevo)  
-        nuevo_b=parametro_prueba     
-        print i
-    else:
-        parametro_beta.append(parametro_beta[i])
-        likehood.append(likehood_viejo)
-        nuevo_b=parametro_prueba
-    i=i+1
-
-
-# In[7]:
-
-plt.figure()
-
-y_final_1=densidad(distancias, parametro[0], parametro[1],parametro[2],parametro[3])
-y_final_2=densidad(distancias, parametro[0], nuevo_b,parametro[2],parametro[3])
-diferencia=loglikelihood(y_obs,y_final_1)-loglikelihood(y_obs,y_final_2)
-
-if (diferencia>0):
-    parametro[1]=nuevo_b
-
+y_final=densidad(distancias, parametro[0], parametro[1],parametro[2],parametro[3])
 print "Parametros finales:"
 print  "Alfa= "+str(parametro[0])+" "
 print  "Beta= "+ str(parametro[1]) +" "
 print  "R0= "+str(np.exp(parametro[2]))+" "
 print  "Rho= "+str(np.exp(parametro[3]))+" "
  
-y_final=densidad(distancias, parametro[0], parametro[1],parametro[2],parametro[3])       
-plt.plot(np.log(distancias),np.log(y_final),label="Modelo")
-plt.scatter(np.log(distancias),np.log(y_obs),label="Observaciones") 
-    
+plt.plot(np.log(distancias),np.log(y_final_1),label="Modelo")
+plt.plot(np.log(distancias),np.log(y_obs),label="Observaciones")     
+       
 plt.xlabel("log(distancia)")
-plt.ylabel("log(densidad)")
-        
+plt.ylabel("log(densidad)")    
 plt.savefig("modelo.pdf")
-
-
-# In[ ]:
-
-
-
-
-# In[ ]:
-
-
-
-
-# In[ ]:
-
-
 
